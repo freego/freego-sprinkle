187c8df06864ce5ed6d9b395f3a48a9f707df08b
Alessandro Lepore <lepore.ale@gmail.com>
2013-06-03 22:20:58 +0200

    adds nginx sample files

--- packages/web_servers/nginx.rb
+++ packages/web_servers/nginx.rb
@@ -5,12 +5,22 @@ package :nginx, :provides => :web_server do
   description 'NGINX web server'
 
   apt 'nginx' do
+    # updated nginx distribution
     pre :install, "wget http://nginx.org/keys/nginx_signing.key && apt-key add ./nginx_signing.key && rm -f nginx_signing.key"
     pre :install, "add-apt-repository 'deb http://nginx.org/packages/ubuntu/ precise nginx'"
     pre :install, 'apt-get update'
   end
 
+  runner 'mkdir -p /etc/nginx/sites-available'
+  runner 'mkdir -p /etc/nginx/sites-enabled'
+  transfer './configurations/nginx/nginx.conf.example', '/etc/nginx/'
+  transfer './configurations/nginx/rails_app.example', '/etc/nginx/sites-available/'
+
   verify do
     has_apt 'nginx'
+    has_file '/etc/nginx/nginx.conf.example'
+    has_file '/etc/nginx/sites-available/rails_app.example'
+    has_directory '/etc/nginx/sites-available'
+    has_directory '/etc/nginx/sites-enabled'
   end
 end
