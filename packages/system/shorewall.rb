a848cb9e44c14d7b84aa3e35bc5f4ad28df37473
Alessandro Lepore <lepore.ale@gmail.com>
2013-06-03 21:43:01 +0200

    adds shorewall firewall

---
+++ packages/system/shorewall.rb
@@ -0,0 +1,26 @@
+#
+# NOTE:
+# you need to manually check and enable configurations in /etc/shorewall
+#
+package :shorewall do
+
+  description 'The shorewall firewall'
+
+  files = %w{interfaces policy rules zones}
+
+  apt 'shorewall'
+
+  files.each do |file|
+    transfer "./configurations/shorewall/#{file}", '/etc/shorewall/'
+  end
+  replace_text "startup=0", "startup=1", "/etc/default/shorewall"
+
+  verify do
+    has_apt "shorewall"
+    files.each do |file|
+      has_file "/etc/shorewall/#{file}"
+    end
+    file_contains "/etc/default/shorewall", "startup=1"
+  end
+
+end
