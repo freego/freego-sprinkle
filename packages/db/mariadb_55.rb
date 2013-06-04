24e68e3a2dfa33f309ce0eed1c7e803ed58a7235
Alessandro Lepore <lepore.ale@gmail.com>
2013-06-04 08:50:24 +0200

    adds mysql sample conf

---
+++ packages/db/mariadb_55.rb
@@ -0,0 +1,18 @@
+package :mariadb_55, :provides => :mysql do
+  description 'MariaDB 5.5 Database'
+
+  apt 'python-software-properties'
+  runner 'apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db'
+  runner "add-apt-repository 'http://mirror.netcologne.de/mariadb/repo/5.5/ubuntu'"
+  
+  apt 'mariadb-server' do
+    pre :install, 'apt-get update'
+  end
+
+  transfer './configurations/mysql/my.cnf.example', '/etc/mysql/'
+
+  verify do
+    has_apt 'mariadb-server'
+    has_file '/etc/mysql/my.cnf.example'
+  end  
+end
