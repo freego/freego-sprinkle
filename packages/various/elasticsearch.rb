f8d6244b2070711ffaa6cdd68d1598dc2d43cf5b
Alessandro Lepore <lepore.ale@gmail.com>
2013-06-04 09:17:04 +0200

    adds elasticsearch

--- packages/various/elasticsearch.rb
+++ packages/various/elasticsearch.rb
@@ -3,4 +3,13 @@
 #
 package :elasticsearch do
 
+  package_name = 'elasticsearch-0.90.1.deb'
+
+  runner "wget https://download.elasticsearch.org/elasticsearch/elasticsearch/#{package_name}"
+  runner "dpkg -i #{package_name}"
+  runner "rm -f #{package_name}"
+
+  verify do
+    has_apt "elasticsearch"
+  end
 end
