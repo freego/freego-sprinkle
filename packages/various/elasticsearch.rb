c9a06f26d2d9bbff4d2748bc47d499cc4f3e851a
Alessandro Lepore <lepore.ale@gmail.com>
2013-06-04 09:19:44 +0200

    fixes elasticsearch (to be tested)

--- packages/various/elasticsearch.rb
+++ packages/various/elasticsearch.rb
@@ -4,6 +4,11 @@
 package :elasticsearch do
 
   package_name = 'elasticsearch-0.90.1.deb'
+  packages = %w{openjdk-7-jre-headless}
+
+  apt packages do
+    pre :install, 'apt-get update'
+  end
 
   runner "wget https://download.elasticsearch.org/elasticsearch/elasticsearch/#{package_name}"
   runner "dpkg -i #{package_name}"
@@ -11,5 +16,6 @@ package :elasticsearch do
 
   verify do
     has_apt "elasticsearch"
+    packages.each { |p| has_apt p }
   end
 end
