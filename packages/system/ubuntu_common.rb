2357c9e34fa4d79e0196727fd4e4bd2fa6128802
Alessandro Lepore <lepore.ale@gmail.com>
2013-08-03 01:55:07 +0200

    use ufw firewall instead of shorewall

--- packages/system/ubuntu_common.rb
+++ packages/system/ubuntu_common.rb
@@ -3,14 +3,18 @@
 #
 package :ubuntu_common do
 
-  packages = %w{ubuntu-minimal wget screen curl vim openssh-client openssh-server 
-                bash-completion logrotate landscape-common links2}
+  packages = %w{ubuntu-minimal wget screen curl vim openssh-client openssh-server
+                bash-completion logrotate landscape-common links2 ufw}
 
   noop do
     pre :install, 'apt-get update'
   end
 
   apt packages
+
+  runner 'ufw allow 80'
+  runner 'ufw allow 22'
+  runner 'ufw enable'
 
   verify do
     packages.each { |p| has_apt p }
