b0f7f8a64713cbf2802a8261d7f57d15ede521d8
Alessandro Lepore <lepore.ale@gmail.com>
2013-11-18 08:23:30 +0100

    use mongodb-10gen for mongodb

--- packages/db/mongodb.rb
+++ packages/db/mongodb.rb
@@ -1,7 +1,10 @@
 package :mongodb do
   description 'MongoDB Database'
 
-  packages = %w{mongodb}
+  packages = %w{mongodb-10gen}
+
+  runner "sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10"
+  runner "echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list"
 
   noop do
     pre :install, 'apt-get update'
